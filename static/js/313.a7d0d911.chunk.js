2c1b7c2a8e5f9e25302e3a0d80a92fef06505678
meylis7 <meylisg98@gmail.com>
2024-03-01 22:23:14 +0500

    Updates

---
+++ static/js/313.a7d0d911.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_tps_storage=self.webpackChunkreact_tps_storage||[]).push([[313],{313:(e,s,t)=>{t.r(s),t.d(s,{default:()=>d});var r=t(791),n=t(911),a=(t(812),t(965)),c=t(184);const l=r.lazy((()=>t.e(816).then(t.bind(t,816)))),d=e=>{let{component:s,...t}=e;return(0,c.jsx)(n.AW,{...t,render:e=>(0,c.jsxs)("div",{children:[(0,c.jsx)(r.Suspense,{fallback:(0,c.jsx)(a.Z,{}),children:(0,c.jsx)(l,{})}),(0,c.jsx)(s,{...e})]})})}}}]);
+//# sourceMappingURL=313.a7d0d911.chunk.js.map
